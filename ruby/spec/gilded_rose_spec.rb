0f73cc42b11ebaa4eb6e7c226b8d8459fc43fef9
russellvaughan <russellvaughan@gmail.com>
2016-04-05 15:28:03 +0100

    first test passes

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -2,9 +2,9 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("foo", 0, 0)] #created an item with name 'foo', sell_in '0', quality'0'
       GildedRose.new(items).update_quality()
-      items[0].name.should == "fixme"
+      expect(items[0].name).to eq "foo"
     end
   end
 
